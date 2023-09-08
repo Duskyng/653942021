73283dc017dffb375ba8c78a957d6322cf7ed89d
Duskyng <47837094+Duskyng@users.noreply.github.com>
2023-09-08 01:29:11 -0300

    Update 04.c

---
+++ C/05092023/lista_exercicios/04.c
@@ -0,0 +1,3 @@
+//4) Faca um programa que possua uma funcao para
+//ler 2 notas e retorna-las por paraˆmetro (chamar uma funcao dedicada a ler 2 notas validas e que devolver os 2 numeros lidos);
+//• calcular a media simples e a media ponderada e retorna-las por parametro, onde a segunda nota tem peso 2 media ponderada = (n1 + n2 ∗ 2)/3;
