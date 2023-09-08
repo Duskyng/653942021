2decb29653e96c21eb78d074ec58edcc199aa564
Duskyng <47837094+Duskyng@users.noreply.github.com>
2023-09-08 01:28:50 -0300

    Update 03.c

---
+++ C/05092023/lista_exercicios/03.c
@@ -0,0 +1,3 @@
+// 3) Escreva uma funcao que dado um numero real passado como parametro, retorne a parte inteira e a parte fracionaria deste numero. Escreva um programa que chama esta funcao. Prototipo:
+
+void  frac(float  num,  int*  inteiro,  float*  frac);
