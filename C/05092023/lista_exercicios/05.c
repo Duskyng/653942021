b908661932e056f53addaea66c11f9db4f1d6883
Duskyng <47837094+Duskyng@users.noreply.github.com>
2023-09-10 20:30:29 -0300

    Update 05.c

--- C/05092023/lista_exercicios/05.c
+++ C/05092023/lista_exercicios/05.c
@@ -1,1 +1,20 @@
 //5) Crie um programa que contenha um array de float contendo 10 elementos. Imprima o enderec¸o de cada posicao desse array.
+
+#include <stdio.h>
+
+int main() {
+    float array[10];
+
+    // Solicite ao usuário inserir valores no array
+    for (int x = 0; x < 10; x++) {
+        printf("Digite um valor para a posição %d: ", x);
+        scanf("%f", &array[x]);
+    }
+
+    // Imprima os endereços de cada posição do array
+    for (int x = 0; x < 10; x++) {
+        printf("Endereço do elemento %d: %p\n", x, (void *)&array[x]);
+    }
+
+    return 0;
+}
