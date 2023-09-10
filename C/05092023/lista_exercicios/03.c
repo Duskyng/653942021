2114cac7bf0ea96ae30816d39235f5bb99224379
Duskyng <47837094+Duskyng@users.noreply.github.com>
2023-09-10 13:00:18 -0300

    Update 03.c

--- C/05092023/lista_exercicios/03.c
+++ C/05092023/lista_exercicios/03.c
@@ -1,3 +1,26 @@
 // 3) Escreva uma funcao que dado um numero real passado como parametro, retorne a parte inteira e a parte fracionaria deste numero. Escreva um programa que chama esta funcao. Prototipo:
 
-void  frac(float  num,  int*  inteiro,  float*  frac);
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+    float num, inteiro;
+    int x = 0, aux;
+
+    printf("Digite um numero real: ");
+    scanf("%f", &num);
+
+    aux = num - x;
+
+    if (aux > 1) {
+        x++;
+    }
+
+    inteiro = aux;
+    printf("%0.f\n", inteiro);
+    
+    printf("%f\n", num - inteiro);
+
+    return 0;
+}
